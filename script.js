217129bc31cba6bef4a087f22f60987303b1c53a
KingMadhav <varshmadhav88@gmail.com>
2024-10-17 23:04:40 +0530

    foyrth comit

--- script.js
+++ script.js
@@ -10,46 +10,46 @@ function loadingAnimation() {
         duration: 0.6,
         delay: 0.5
     }),
-    tl.from(".line1-part1,.line h2", {
-        opacity: 0,
-        onStart: function () {
-            h5timer = document.querySelector(".line1-part1 h5"),
-            grow = 0,
-            setInterval(function () {
-                if (grow < 100) {
-                    h5timer.innerHTML = grow++
-                }
-                else {
-                    h5timer.innerHTML = grow
-                }
-            }, 35)
-        }
-    }),
-    tl.to(".line h2", {
-        animationName: "loaderanime",
-        opacity: 1
-    }),
+        tl.from(".line1-part1,.line h2", {
+            opacity: 0,
+            onStart: function () {
+                h5timer = document.querySelector(".line1-part1 h5"),
+                    grow = 0,
+                    setInterval(function () {
+                        if (grow < 100) {
+                            h5timer.innerHTML = grow++
+                        }
+                        else {
+                            h5timer.innerHTML = grow
+                        }
+                    }, 35)
+            }
+        }),
+        tl.to(".line h2", {
+            animationName: "loaderanime",
+            opacity: 1
+        }),
 
-    tl.to(".loader", {
-        opacity: 0,
-        duration: 0.2,
-        delay: 3.2
-    }),
-    tl.from(".page1", {
-        y: 1600,
-        delay: 0.2,
-        ease: Power4,
-        duration: 0.5,
-        opacity: 0
-    })
+        tl.to(".loader", {
+            opacity: 0,
+            duration: 0.2,
+            delay: 3.2
+        }),
+        tl.from(".main", {
+            y: 1000,
+            delay: 0.2,
+            ease: Power4,
+            duration: 0.5,
+            opacity: 0
+        })
 }
 
-// loadingAnimation();
+loadingAnimation();
 document.addEventListener("mousemove",
-    function (dets){
-        gsap.to(".crsr",{
-            left:dets.x,
-            top:dets.y,
+    function (dets) {
+        gsap.to(".crsr", {
+            left: dets.x,
+            top: dets.y,
         })
-    
-})
+
+    })
